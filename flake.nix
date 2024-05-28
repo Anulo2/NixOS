7acd3471f8f71f2a382c5261b6f0c3f88a8552ee
anulo2 <leonardocanello@protonmail.com>
2024-05-28 17:59:56 +0200

    added some absic softwares

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "_Zaizen_ NixOS configuration";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }: 
+  
+  let 
+    system = "x86_64-linux";
+
+    pkgs = import nixpkgs {
+      inherit system;
+      
+      config = {
+        allowUnfree = true;
+      };
+    };
+    in
+  {
+      nixosConfigurations = {
+        anulo2Nixos = nixpkgs.lib.nixosSystem {
+          specialArgs = {inherit system;};
+
+          modules = [
+            ./nixos/configuration.nix
+          ];
+        };
+      };
+  };
+}
